1cc0d2fe5ad1a00c755b8630850eb6b6da66c66c
ahmedtelbani <ahmedeltelbani@outlook.com>
2024-08-12 19:03:59 +0300

    Add User and Meal Data Model.

---
+++ app/src/main/java/com/example/recipeapp/data/model/ResponseObject.kt
@@ -0,0 +1,5 @@
+package com.example.recipeapp.data.model
+
+data class ResponseObject(
+    val meals: List<Meal>
+)
--- app/src/main/java/com/example/recipeapp/data/model/User.kt
+++ app/src/main/java/com/example/recipeapp/data/model/User.kt
@@ -1,5 +1,14 @@
 package com.example.recipeapp.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class User(
-    val id: Int,
+    val name: String,
+    val email: String,
+    val hashedPassword: String,
+    val lastLoginTimestamp: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0
 )
