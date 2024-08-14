b0267ea0b3033049d8bcc48b9bd051821ad8219a
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-14 15:34:59 +0300

    removed Old models

--- app/src/main/java/com/example/recipeapp/network/api/responses/MultiMealResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.recipeapp.network.api.responses
-
-import com.example.recipeapp.network.api.MealDTO
-
-data class MultiMealResponse(
-    val meals: List<MealDTO>? = emptyList()
-)
