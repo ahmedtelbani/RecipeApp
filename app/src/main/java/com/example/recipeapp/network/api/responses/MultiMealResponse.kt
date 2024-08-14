2d235de0b50d966a33efd1cef75f4d5959ec111e
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-14 12:25:22 +0300

    updated the response that in some cases comes with `null` value

--- app/src/main/java/com/example/recipeapp/network/api/responses/MultiMealResponse.kt
+++ app/src/main/java/com/example/recipeapp/network/api/responses/MultiMealResponse.kt
@@ -3,5 +3,5 @@ package com.example.recipeapp.network.api.responses
 import com.example.recipeapp.network.api.MealDTO
 
 data class MultiMealResponse(
-    val meals: List<MealDTO> = emptyList()
+    val meals: List<MealDTO>? = emptyList()
 )
