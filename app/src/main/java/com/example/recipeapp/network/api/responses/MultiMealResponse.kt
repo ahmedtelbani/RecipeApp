5a5dcbb820decfa2f43a7bb1d0da330f05f18e47
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-14 12:22:47 +0300

    data model for the response coming from the server

---
+++ app/src/main/java/com/example/recipeapp/network/api/responses/MultiMealResponse.kt
@@ -0,0 +1,7 @@
+package com.example.recipeapp.network.api.responses
+
+import com.example.recipeapp.network.api.MealDTO
+
+data class MultiMealResponse(
+    val meals: List<MealDTO> = emptyList()
+)
