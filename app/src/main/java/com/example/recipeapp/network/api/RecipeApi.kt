ac60b28d949bbb1d6342eac6baa739cc4d5051a6
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-14 12:35:43 +0300

    added implementation of the api

--- app/src/main/java/com/example/recipeapp/network/api/RecipeApi.kt
+++ app/src/main/java/com/example/recipeapp/network/api/RecipeApi.kt
@@ -1,7 +1,25 @@
 package com.example.recipeapp.network.api
 
+import com.example.recipeapp.network.api.responses.MultiMealResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface RecipeApi {
     /**
     * Interface defining Retrofit API endpoints.
     */
+
+    // DON'T USE THIS FUNCTION NOW
+    @GET("")
+    suspend fun getAllMeals()
+
+
+    @GET("lookup.php")
+    suspend fun getMealById(@Query("i")  mealId: Int): MultiMealResponse
+
+
+    @GET("search.php")
+    suspend fun searchMealsByName(@Query("s") mealName: String): MultiMealResponse
+
+
 }
