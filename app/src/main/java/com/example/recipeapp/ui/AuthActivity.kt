25f8825450046ca7c9e6f49e939d431228a2cfec
isl001 <Islam.Ahmed.53@h-eng.helwan.edu.eg>
2024-08-14 03:06:36 +0300

    Splash screen feat

--- app/src/main/java/com/example/recipeapp/ui/AuthActivity.kt
+++ app/src/main/java/com/example/recipeapp/ui/AuthActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.recipeapp.R
 import com.example.recipeapp.ui.viewmodel.AuthViewModel
 
@@ -16,16 +18,15 @@ class AuthActivity : AppCompatActivity() {
      */
 
     private val authViewModel: AuthViewModel by viewModels()
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        // TO DO
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_auth) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
--- app/src/main/java/com/example/recipeapp/ui/main/SplashFragment.kt
+++ app/src/main/java/com/example/recipeapp/ui/main/SplashFragment.kt
@@ -4,30 +4,41 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.recipeapp.R
 import com.example.recipeapp.ui.AuthActivity
 import com.example.recipeapp.ui.MainActivity
 import com.example.recipeapp.util.PreferencesHelper
 
+class SplashFragment : Fragment() {
 
-class SplashFragment : AppCompatActivity() {
-    private lateinit var preferencesHelper: PreferencesHelper
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_splash)
-        preferencesHelper = PreferencesHelper(this)
-        val value = preferencesHelper.getValue()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
         Handler(Looper.getMainLooper()).postDelayed({
-            if (value == 0) {
-                val intent = Intent(this, AuthActivity::class.java)
-                startActivity(intent)
+            val navController = findNavController()
+            val preferencesHelper = PreferencesHelper(requireContext())
+            val userStatus = preferencesHelper.getValue()
+
+            if (userStatus == 0) {
+
+                navController.navigate(R.id.action_splashFragment_to_loginFragment)
             } else {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+
+                startActivity(Intent(requireActivity(), MainActivity::class.java))
+                requireActivity().finish()
             }
-            finish()
-        },3000)
+        }, 3000)
     }
 }
