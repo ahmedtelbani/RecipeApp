d7f8b5e583e2648d70efedad8081d4a0b6ef1c23
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-15 13:33:02 +0300

    updated fragment to be ready to implement its logic

--- app/src/main/java/com/example/recipeapp/ui/main/RecipeDetailFragment.kt
+++ app/src/main/java/com/example/recipeapp/ui/main/RecipeDetailFragment.kt
@@ -5,9 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.recipeapp.R
+import com.example.recipeapp.data.model.Meal
+import com.example.recipeapp.ui.viewmodel.RecipeViewModel
 
 class RecipeDetailFragment : Fragment() {
+
+    val args: RecipeDetailFragmentArgs by navArgs()
+    val viewModel: RecipeViewModel by viewModels()
+
+    // UI elements
+
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -17,6 +28,17 @@ class RecipeDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // TO DO
+        // call the api via repository
+        viewModel.getMealById(args.selectedMealId)
+        // observe the results
+        viewModel.selectedRecipe.observe(viewLifecycleOwner) {
+            if(it != null) {
+                updateMealUI(it)
+            }
+        }
     }
 
+    private fun updateMealUI(meal: Meal) {
+        TODO("not implemented yet")
+    }
 }
