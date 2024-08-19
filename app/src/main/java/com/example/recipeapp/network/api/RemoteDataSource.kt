68ccee31a477967b3ba0d223c53c7df94d4a0b1e
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-19 18:35:56 +0300

    updated implementation to use safeApiCall function to handle the request

--- app/src/main/java/com/example/recipeapp/network/api/RemoteDataSource.kt
+++ app/src/main/java/com/example/recipeapp/network/api/RemoteDataSource.kt
@@ -2,23 +2,33 @@ package com.example.recipeapp.network.api
 
 import com.example.recipeapp.data.model.Categories
 import com.example.recipeapp.data.model.Meal
+import com.example.recipeapp.data.model.ResponseObject
+import com.example.recipeapp.network.response.ApiResponse
+import com.example.recipeapp.network.response.safeApiCall
+import com.example.recipeapp.util.isInternetAvailable
+import retrofit2.HttpException
+import java.io.IOException
 
 class RemoteDataSource(
     private val api: RecipeApi
 ): IRemoteDataSource {
-    override suspend fun getAllMeals(): List<Meal> {
-        return api.getAllMeals().meals ?: emptyList()
+    override suspend fun getAllMeals(): ApiResponse {
+        return safeApiCall { api.getAllMeals() }
     }
 
     override suspend fun getCategories(): List<Categories> {
         return api.getCategories().categories
     }
 
-    override suspend fun getMealById(mealId: Int): Meal? {
-        return api.getMealById(mealId).meals?.first()
+    override suspend fun getMealById(mealId: Int): ApiResponse {
+        return safeApiCall { api.getMealById(mealId) }
+//        return api.getMealById(mealId).meals?.first()
     }
 
-    override suspend fun searchMealsByName(mealName: String): List<Meal>? {
-        return api.searchMealsByName(mealName).meals
+    override suspend fun searchMealsByName(mealName: String): ApiResponse {
+        return safeApiCall { api.searchMealsByName(mealName) }
+//        return api.searchMealsByName(mealName).meals
     }
+
+
 }
