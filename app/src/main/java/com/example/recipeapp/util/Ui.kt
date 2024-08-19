03af6d31e06a9a46be893bff0ef3b3c4e303ccb6
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-08-19 18:41:09 +0300

    created new file that handles ui with ApiResponse

---
+++ app/src/main/java/com/example/recipeapp/util/Ui.kt
@@ -0,0 +1,19 @@
+package com.example.recipeapp.util
+
+import com.example.recipeapp.data.model.ResponseObject
+import com.example.recipeapp.network.response.ApiResponse
+
+fun handleApiResponseUiLogic(
+    apiResponse: ApiResponse,
+    onSuccess: (data: ResponseObject) -> Unit = {},
+    onHttpError: (code: Int, message: String) -> Unit = {code, message -> },
+    onUnknownError: (exception: Exception) -> Unit = {},
+    onSomethingElseHappen: () -> Unit = {}
+) {
+    when(apiResponse) {
+        is ApiResponse.Success -> onSuccess(apiResponse.data)
+        is ApiResponse.Error.HttpError -> onHttpError(apiResponse.code, apiResponse.message)
+        is ApiResponse.Error.UnknownError -> onUnknownError(apiResponse.exception)
+        else -> onSomethingElseHappen()
+    }
+}
